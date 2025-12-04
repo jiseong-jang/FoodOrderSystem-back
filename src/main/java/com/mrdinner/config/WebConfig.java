8f40c94cdfd91c1c5bc686745ea91bd59dcd8d00
지성 <727jisung@gmail.com>
2025-12-04 23:26:15 +0900

    feat: 諛깆뿏??CORS ?ㅼ젙 異붽?

--- src/main/java/com/mrdinner/config/WebConfig.java
+++ src/main/java/com/mrdinner/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.mrdinner.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.charset.StandardCharsets;
@@ -14,6 +15,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         StringHttpMessageConverter stringConverter = new StringHttpMessageConverter(StandardCharsets.UTF_8);
         converters.add(stringConverter);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://mr-daeback.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
 
